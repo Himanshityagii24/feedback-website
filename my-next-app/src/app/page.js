6a5952cf4af8bccdf5ecf47b1ef34bb34533b8bd
Himanshi <tyagihimanshi2024@gmail.com>
2024-06-25 17:44:57 +0530

    Frontend for home page done using next.js

---
+++ my-next-app/src/app/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from "next/link";
+import styles from "./page.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Featured from "@/components/featured/Featured";
+import Footer from '@/components/footer/Footer';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+     
+      <Featured />
+      <Footer />
+    </div>
+  );
+}
---
+++ my-next-app/src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+// src/components/featured/Featured.jsx
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, colleagues.</b> Together We Grow: Your Feedback, Our Strength.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/feedback.jpg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Cupiditate, quam nisi magni ea laborum inventore voluptatum
+            laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
+            quisquam! Harum unde sit culpa debitis.
+          </p>
+          <button className={styles.button}>Rate now!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
